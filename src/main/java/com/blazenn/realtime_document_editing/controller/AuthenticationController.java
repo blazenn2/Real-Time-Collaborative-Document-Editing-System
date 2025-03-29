b26cc7691fa2d266ced58dcacfc52c7a88a09e3a
Hamza Nawab <hamzanawab.bs@gmail.com>
2025-03-30 02:41:23 +0500

    Addition of put and patch for app user APIs

--- src/main/java/com/blazenn/realtime_document_editing/controller/AuthenticationController.java
+++ src/main/java/com/blazenn/realtime_document_editing/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import org.apache.coyote.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/authenticate")
--- src/main/java/com/blazenn/realtime_document_editing/service/mapper/AppUserMapper.java
+++ src/main/java/com/blazenn/realtime_document_editing/service/mapper/AppUserMapper.java
@@ -2,7 +2,10 @@ package com.blazenn.realtime_document_editing.service.mapper;
 
 import com.blazenn.realtime_document_editing.dto.AppUserDTO;
 import com.blazenn.realtime_document_editing.model.AppUser;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -12,4 +15,7 @@ public interface AppUserMapper {
     AppUserDTO appUserToAppUserDTO(AppUser appUser);
 
     AppUser appUserDTOToAppUser(AppUserDTO appUserDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    AppUser updateAppUserFromDTO(AppUserDTO appUserDTO, @MappingTarget AppUser appUser);
 }
